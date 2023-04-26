d3f86b005995b507ecefcc8de34d738ecc66c7ff
Fathi Ahmed <fathismael@gmail.com>
2023-04-26 11:05:12 +0300

    airbnb-config, dashboard-layout folder, setting-up redux store, setting-up global theme

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,1 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const inialState = {
+    mode: "dark",
+}
+
+export const globalSlice = createSlice({
+    name: 'global',
+    inialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light"
+        }
+    }
+})
+
+export const { setMode } = globalSlice.actions
+export default globalSlice.reducer
