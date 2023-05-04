e41f3c1e701086b7d5416274d1a59900047d619c
Fathi Ahmed <fathismael@gmail.com>
2023-05-04 08:27:53 +0300

    finished navbar-sidebar, with current user logged in

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -4,22 +4,28 @@ import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import Sidebar from '../../components/Sidebar'
+import { useGetUserQuery } from '../../state/api.js'
 
 const Layout = () => {
 
   const isNonMobile = useMediaQuery("(min-width: 600px)")
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const userId = useSelector((state) => state.global.userId)
+  const { data } = useGetUserQuery(userId)
+
 
   return (
     <Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setIsSidebarOpen}
       />
-      <Box>
+      <Box flexGrow={1}>
         <Navbar
+          user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
